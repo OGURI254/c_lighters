aab04b483792447158c16b0b2f48869ca17969c7
jeff <o.jeff3.a@gmail.com>
2024-11-14 01:26:45 +0300

    add keys

--- city_lighters/settings.py
+++ city_lighters/settings.py
@@ -74,7 +74,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
-
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 CKEDITOR_5_MAX_FILE_SIZE = 5
 CKEDITOR_5_CONFIGS = {
     'default': {
